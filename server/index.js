4b0408945d3e7959dbb1c153c36e2d9b8e6eafe9
farewell7117 <farewell7117@gmail.com>
2020-07-04 20:01:15 +0100

    Initial server side.

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Nitro');
+});
+
+app.get('/posts', (req, res) => {
+  res.setHeader('content-type', 'application/json');
+  fs.createReadStream(`${__dirname}/data/posts.json`).pipe(res);
+});
+
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
