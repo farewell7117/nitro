4e1df7f0b19afd421de43188158ead9abf48d35d
farewell7117 <farewell7117@gmail.com>
2020-07-05 19:37:29 +0100

    Clean up week calculation function.

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -1,16 +1,16 @@
 import { Post } from '../models/post';
 
-export const calculateWeek = (post: Post): [number, number] => {
-  const d = new Date(Number(post.time) * 1000);
+export const calculateWeek = (time: string): [number, number] => {
+  const d = new Date(Number(time) * 1000);
   const [year, month, date, day] = [d.getFullYear(), d.getMonth(), d.getDate(), d.getDay()];
 
-  const startDate = new Date(year, month, date - day + 1);
-  const endDate = new Date(year, month, date - day + 7, 23, 59, 59, 999);
+  const startDate = new Date(year, month, date - day + 1); // Monday
+  const endDate = new Date(year, month, date - day + 7, 23, 59, 59, 999); // Sunday
 
   return [startDate.getTime(), endDate.getTime()];
 };
 
 export const preprocess = (posts: any[]): Post[] => posts.map((post) => ({
   ...post,
-  week: calculateWeek(post),
+  week: calculateWeek(post.time),
 }));
